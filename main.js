ff078644263fe8ac83e703f1fe797e3191614b4e
dogeowsky <cptcombinedogepl@gmail.com>
2025-03-01 02:02:27 +0100

    Few changes
    - addition of meow.mp3 :3
    - added a cool button (https://itsoktawian.net)
    - few css changes

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 setInterval(() => {
     document.querySelector(".author").style.color = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`
 }, 1000)
+
+const meow_audio = new Audio('audio/meow.mp3')
+
+document.querySelector(".author").addEventListener("click", () => {
+    meow_audio.play()
+})
