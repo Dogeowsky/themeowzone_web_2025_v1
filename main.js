95b83321bb32614b2531d726b6090fe7c6ff3379
dogeowsky <cptcombinedogepl@gmail.com>
2025-03-01 02:57:11 +0100

    Another batch of changes

--- main.js
+++ main.js
@@ -7,3 +7,5 @@ const meow_audio = new Audio('audio/meow.mp3')
 document.querySelector(".author").addEventListener("click", () => {
     meow_audio.play()
 })
+
+document.querySelector("#my_button").textContent = `<a href="https://themeowzone.net/" target="_parent"><img src="https://themeowzone.net/files/button.png" alt="TheMeowZone" title="mewo >w<"></a>`
