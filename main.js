cf1c439a5f95e4246e3e348e82550d7a524e3dbd
dogeowsky <cptcombinedogepl@gmail.com>
2025-03-14 15:41:09 +0100

    More meows!

--- main.js
+++ main.js
@@ -2,17 +2,24 @@ setInterval(() => {
     document.querySelector(".author").style.color = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`
 }, 1000)
 
-const meow_audio = new Audio('audio/meow.mp3')
-
 document.querySelector(".author").addEventListener("click", () => {
+    const meows = ["audio/meow_1.mp3", "audio/meow_2.mp3", "audio/meow_3.mp3"]
+    let random_meow = Math.floor(Math.random() * 3)
+    let meow_audio = new Audio(meows[random_meow])
+    
+    console.log(`Playing sound: ${meows[random_meow]}`)
     meow_audio.play()
 })
 
 const buttonSnippet = `<a href="https://themeowzone.net/" target="_parent"><img src="https://themeowzone.net/files/button.png" alt="TheMeowZone" title="mewo >w<"></a>`
 
-document.querySelector("#copy_button").addEventListener("click", () => {
-     navigator.clipboard.writeText(buttonSnippet)
-     alert("Copied the snippet!")
-})
+if(document.URL == "https://themeowzone.net/about.html") {
+    document.querySelector("#copy_button").addEventListener("click", () => {
+        navigator.clipboard.writeText(buttonSnippet)
+        alert("Copied the snippet!")
+   })
+   
+   document.querySelector("#my_button").textContent = buttonSnippet
+}
 
-document.querySelector("#my_button").textContent = buttonSnippet
+
