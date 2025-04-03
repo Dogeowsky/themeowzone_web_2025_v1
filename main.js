cbf91d39dddb660210d72b7b87950d8b6b61c722
dogeowsky <77202347+Dogeowsky@users.noreply.github.com>
2025-04-04 00:20:42 +0200

    Modified html head, remade author span with css keyframes

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-setInterval(() => {
-    document.querySelector(".author").style.color = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`
-}, 1000)
-
 document.querySelector(".author").addEventListener("click", () => {
     const meows = ["audio/meow_1.mp3", "audio/meow_2.mp3", "audio/meow_3.mp3"]
     let random_meow = Math.floor(Math.random() * 3)
@@ -11,9 +7,10 @@ document.querySelector(".author").addEventListener("click", () => {
     meow_audio.play()
 })
 
-const buttonSnippet = `<a href="https://themeowzone.net/" target="_parent"><img src="https://themeowzone.net/files/button.png" alt="TheMeowZone" title="mewo >w<"></a>`
 
 if(document.URL == "https://themeowzone.net/about.html") {
+    const buttonSnippet = `<a href="https://themeowzone.net/" target="_parent"><img src="https://themeowzone.net/files/button.png" alt="TheMeowZone" title="mewo >w<"></a>`
+    
     document.querySelector("#copy_button").addEventListener("click", () => {
         navigator.clipboard.writeText(buttonSnippet)
         alert("Copied the snippet!")
